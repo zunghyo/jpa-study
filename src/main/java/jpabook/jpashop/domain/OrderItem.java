e3457bc203afaedba195430dccd0c64efbaaaba5
hyojeong <khj444453@gmail.com>
2022-11-03 17:12:11 +0900

    First Commit

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private long id;
+
+    @Column(name = "ORDER_ID")
+    private long orderId;
+
+    @Column(name = "ITEM_ID")
+    private long itemId;
+
+    private int orderPrice;
+    private int count;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
