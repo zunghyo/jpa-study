c047961f29adeb67e5eefc008fb08b9efaa54925
hyojeong <khj444453@gmail.com>
2022-11-05 14:59:39 +0900

    다양한 연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -28,6 +28,10 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     public Long getId() {
         return id;
     }
